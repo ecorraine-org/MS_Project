cfdabd270e72672c650e8593dd9f474e4e44b160
893aiueo <95463894+893aiueo@users.noreply.github.com>
2024-10-04 17:33:23 +0900

    Fix ScriptName
    CameraFollowからCameraControllerに名前変更
    HitDetectionCameraの下準備

---
+++ MS_Project/Assets/Scripts/Camera/HitDetectionSystem.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// ヒット判定補正用のクラス
+/// </summary>
+public class HitDetectionSystem : MonoBehaviour
+{
+
+}
