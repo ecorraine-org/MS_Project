77848b305b42ea94dba820d091f1ec1bc10baace
zxc148995 <zxc148995@163.com>
2024-10-04 11:43:38 +0900

    Update: オノマトペを食べる簡易処理

--- MS_Project/Assets/Scripts/Character/Player/State/PlayerEatState.cs
+++ MS_Project/Assets/Scripts/Character/Player/State/PlayerEatState.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class PlayerEatState : PlayerState
 {
+    //捕食test
+    public UnityEngine.Vector3 attackSize = new UnityEngine.Vector3(1f, 1f, 1f);
+    UnityEngine.Vector3 attackAreaPos;
+    public UnityEngine.Vector3 offsetPos;
+    public float attackDamage;
+    public LayerMask onomatoLayer;
+
     public override void Init(PlayerController _playerController)
     {
         base.Init(_playerController);
         Debug.Log("捕食ステート");
+
+        Attack();
+        spriteAnim.Play("Attack");
     }
 
     public override void Tick()
     {
-        playerController.StateManager.TransitionState(StateType.Idle);
+
+        //アニメーション終了、アイドルへ遷移
+        if (spriteAnim.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1f)
+        {
+            playerController.StateManager.TransitionState(StateType.Idle);
+        }
     }
 
     public override void FixedTick()
@@ -23,5 +38,29 @@ public class PlayerEatState : PlayerState
     public override void Exit()
     {
 
+    }
+
+    public void Attack()
+    {
+
+        attackAreaPos = transform.position;
+
+        //左右反転か
+        offsetPos.x = spriteRenderer.flipX ? Mathf.Abs(offsetPos.x) : -Mathf.Abs(offsetPos.x);
+
+        attackAreaPos += offsetPos;
+
+        //コライダーの検出
+        playerController.AttackCollider.DetectColliders(attackAreaPos, attackSize, attackDamage, onomatoLayer);
+
+    }
+
+    /// <summary>
+    /// 描画test
+    /// </summary>
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(attackAreaPos, attackSize);
     }
 }
