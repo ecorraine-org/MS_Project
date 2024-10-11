8321921207c4e8f955bd19a23292d2987c63561c
893aiueo <95463894+893aiueo@users.noreply.github.com>
2024-10-11 15:38:36 +0900

    Add Assets Scenes Prefab
    #1
    Top-DownFantasyAssets
    FantasyAdventureEnvironment
    の2つのAssets追加
    #2
    ScenesフォルダにSceneStreamerを利用したStage01とStaege02の追加
    StartScene01をPlayでシーンのロード(仮実装)
    #3
    BuildSettingsの変更
    Sceneロードを実装するために必要なステージをBuildSettingに追加

--- MS_Project/Assets/Plugins/Pixel Crushers/Scene Streamer/Scripts/SetStartScene.cs
+++ MS_Project/Assets/Plugins/Pixel Crushers/Scene Streamer/Scripts/SetStartScene.cs
@@ -7,8 +7,10 @@ namespace PixelCrushers.SceneStreamer
 	/// <summary>
 	/// Sets the current scene at Start().
 	/// </summary>
+	/// 
+	//[SerializeField, Header("開始時のプレイヤー")] private GameObject _player;
 	[AddComponentMenu("Scene Streamer/Set Start Scene")]
-	public class SetStartScene : MonoBehaviour 
+	public class SetStartScene : MonoBehaviour
 	{
 
 		/// <summary>
@@ -17,9 +19,10 @@ namespace PixelCrushers.SceneStreamer
 		[Tooltip("Load this scene at start")]
 		public string startSceneName = "Scene 1";
 
-		public void Start() 
+		public void Awake()
 		{
 			SceneStreamer.SetCurrentScene(startSceneName);
+			//_player = GameObject.Find("Player");
 			Destroy(this);
 		}
 
