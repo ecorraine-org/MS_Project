51f87b4982b14e9f322713799dee5356d396facc
893aiueo <aiueoon700@gmail.com>
2024-11-14 16:02:50 +0900

    update Camera,Enum
    *EnumにcameraTypeを追加
    *ShakeDataを再設計

---
+++ MS_Project/Assets/Scripts/Camera/CameraSettings.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// カメラの基本設定
+/// </summary>
+[CreateAssetMenu(fileName = "CameraSettings", menuName = "ScriptableObject/Camera/Settings")]
+public class CameraSettings : ScriptableObject
+{
+
+}
