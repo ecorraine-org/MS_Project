b091c119f7b750fd6221f394388ab38a03a5683d
ohs10373 <114590340+ohs10373@users.noreply.github.com>
2024-11-21 00:58:23 +0900

    AI制作
    ボスのAIに着手
    歩行中時間経過でダッシュ
    ダッシュ中の攻撃は2回切りつける

--- MS_Project/Assets/Model/09_Ultimate/AIUltimate.cs
+++ MS_Project/Assets/Model/09_Ultimate/AIUltimate.cs
@@ -7,31 +7,32 @@ using UnityEngine.Playables; //PlayableDirectorを使うための宣言
 
 public class AIUltimate : EnemyAction
 {
-
-
-    [SerializeField, Header("突進スピード")]
-    public float chargeSpeed = 60.0f;
     private Animator anim;
+    private AnimatorStateInfo stateInfo;// 現在のアニメーション状態
 
     //　キャラ状態の定義
-    public enum StateNo
+    public enum State
     {
-        Idle,
-        Chase,
-        Attack,
-        StateEnd
+        IDLE,
+        WALK,
+        DASH,
+        SLASH,
+        SLASH_DUAL,
+        STATEEND
     };
-    StateNo state = 0;
+    State stateNo = 0;
+    private bool ctrl = true;
 
     private float maxDistance;
     private float minDistance;
 
-    float frameTime = 0.0f; //時間計測
-
+    private float frameTime = 0.0f; //時間計測
+    private Vector3 direction;
 
     protected override void Start()
     {
         base.Start();
+        anim = GetComponentInChildren<Animator>();
 
         maxDistance = enemy.EnemyStatus.StatusData.attackDistance;
         minDistance = enemy.EnemyStatus.StatusData.attackDistance / 1.3f;
@@ -39,18 +40,63 @@ public class AIUltimate : EnemyAction
 
     private void Update()
     {
+        //距離チェック
         distanceToPlayer = Vector3.Distance(player.position, enemy.transform.position);
 
 
-        if (distanceToPlayer < minDistance)
-        {
-            
+        // 現在のアニメーション状態を取得
+        stateInfo = anim.GetCurrentAnimatorStateInfo(0);
 
+
+        //射程にいるかどうか
+        if (ctrl && distanceToPlayer <= EnemyStatus.StatusData.chaseDistance)
+        {
+
+            if (distanceToPlayer <= EnemyStatus.StatusData.attackDistance)
+            {
+                stateNo = State.SLASH;
+            }
+            else if (stateNo == State.IDLE)
+            {
+                stateNo = State.WALK;
+            }
         }
         else
         {
-            anim.Play("Damaged");
+            if (enemy.AllowAttack || enemy.IsAttacking)
+            {
+                enemy.AllowAttack = false;
+                enemy.IsAttacking = false;
+            }
+
+            // 停止
+            enemy.OnMovementInput?.Invoke(Vector3.zero);
         }
+
+
+        //デバッグ
+        Debug.Log(stateNo);
+        
+        // 状態ごとの行動
+        switch (stateNo)
+        {
+            case State.IDLE:
+                Idle();
+                break;
+            case State.WALK:
+                Walk();
+                break;
+            case State.DASH:
+                Dash();
+                break;
+            case State.SLASH:
+                Slash();
+                break;
+            case State.SLASH_DUAL:
+                Slash_Dual();
+                break;
+        }
+
 
         //　敵の向きをプレイヤーの方向に少しづつ変える
         //var dir = (GetDestination() - transform.position).normalized;
@@ -62,28 +108,40 @@ public class AIUltimate : EnemyAction
 
     public override void Chase()
     {
-        frameTime += Time.deltaTime; //時間計測
-        Debug.Log(frameTime);
-        if (frameTime >= 10.0f)
+        
+    }
+
+    public void Idle()
+    {
+        stateNo = State.IDLE;
+        ctrl = true;
+    }
+
+    public void Walk()
+    {
+        stateNo = State.WALK;
+        anim.Play("Walk");//歩こう
+        ctrl = true;
+
+        frameTime++; //時間計測
+
+        //しばらく歩いて走りたいかも
+        //Debug.Log(frameTime);
+        if (frameTime >= 50.0f &&
+            distanceToPlayer <= EnemyStatus.StatusData.chaseDistance)
         {
-            Dash();
+            Dash();//走ろう
+            return;
         }
 
         if (distanceToPlayer <= EnemyStatus.StatusData.chaseDistance)
         {
-            Vector3 direction = player.position - enemy.transform.position;
-            // 進む方向に向く
-            Quaternion forwardRotation = Quaternion.LookRotation(direction.normalized);
-            forwardRotation.x = 0f;
-            enemy.transform.rotation = forwardRotation;
+            Looking();
 
             if (distanceToPlayer <= EnemyStatus.StatusData.attackDistance)
             {
-                enemy.AllowAttack = true;
-                enemy.OnMovementInput?.Invoke(Vector3.zero);
-
-                // 攻撃
-                enemy.OnAttack?.Invoke();
+                stateNo = State.SLASH;
+                return;
             }
             else
             {
@@ -113,24 +171,22 @@ public class AIUltimate : EnemyAction
 
     public void Dash()
     {
+        stateNo = State.DASH;
+        ctrl = false;
+
         if (distanceToPlayer <= EnemyStatus.StatusData.chaseDistance)
         {
-            Vector3 direction = player.position - enemy.transform.position;
-            // 進む方向に向く
-            Quaternion forwardRotation = Quaternion.LookRotation(direction.normalized);
-            forwardRotation.x = 0f;
-            enemy.transform.rotation = forwardRotation;
+            Looking();
 
             if (distanceToPlayer <= EnemyStatus.StatusData.attackDistance)
             {
-                enemy.AllowAttack = true;
-                enemy.OnMovementInput?.Invoke(Vector3.zero);
-
-                // 攻撃
-                enemy.OnAttack?.Invoke();
+                //攻撃
+                stateNo = State.SLASH_DUAL;
+                return;
             }
             else
             {
+                anim.Play("Dash");//走ろう
                 if (enemy.AllowAttack || enemy.IsAttacking)
                 {
                     enemy.AllowAttack = false;
@@ -138,7 +194,7 @@ public class AIUltimate : EnemyAction
                 }
 
                 // 追跡
-                enemy.OnMovementInput?.Invoke(direction.normalized * 5);
+                enemy.OnMovementInput?.Invoke(direction.normalized * 7);
             }
         }
         else
@@ -155,28 +211,76 @@ public class AIUltimate : EnemyAction
 
     }
 
+    public void Slash()
+    {
+        stateNo = State.SLASH;
+        anim.Play("Slash");
+        ctrl = false;
+
+        // 停止
+        enemy.OnMovementInput?.Invoke(Vector3.zero);
+        //アニメーション停止
+        if (stateInfo.IsName("Slash") && stateInfo.normalizedTime >= 1.0f)
+        {
+            frameTime = 0.0f;
+            stateNo = State.IDLE;
+            Debug.Log("アニメーション 'Slash' が終了しました！");
+        }
+        //enemy.OnMovementInput?.Invoke(Vector3.zero);
+    }
+
+    public void Slash_Dual()
+    {
+        stateNo = State.SLASH_DUAL;
+        anim.Play("Slash_Dual");
+        ctrl = false;
+
+        // 停止
+        enemy.OnMovementInput?.Invoke(Vector3.zero);
+
+        //アニメーション終了
+        if (stateInfo.IsName("Slash_Dual") && stateInfo.normalizedTime >= 1.0f)
+        {
+            frameTime = 0.0f;
+            stateNo = State.IDLE;
+            Debug.Log("アニメーション 'Slash_Dual' が終了しました！");
+        }
+        //enemy.OnMovementInput?.Invoke(Vector3.zero);
+    }
+
     //　タイムラインで状態をStateEnd状態に設定するためのメソッド
     public void EndState()
     {
-        SetState(StateNo.StateEnd); ;
+        SetState(State.STATEEND); ;
     }
 
     //　敵キャラの状態を設定するためのメソッド 
-    public void SetState(StateNo tempState, Transform targetObject = null)
+    public void SetState(State tempState, Transform targetObject = null)
     {
-        state = tempState;
+        stateNo = tempState;
 
-        if (tempState == StateNo.Idle)
+        if (tempState == State.IDLE)
         {
             //navMeshAgent.isStopped = true; //キャラの移動を止める
             //animator.SetBool("chase", false); //アニメーションコントローラーのフラグ切替（Chase⇒Idle）
         }
     }
 
-    //　敵キャラの状態を取得するためのメソッド
-    public StateNo GetState()
+
+    //見ているとこ見る
+    public void Looking()
     {
-        return state;
+        direction = player.position - enemy.transform.position;
+        // 進む方向に向く
+        Quaternion forwardRotation = Quaternion.LookRotation(direction.normalized);
+        forwardRotation.x = 0f;
+        enemy.transform.rotation = forwardRotation;
+    }
+
+    //　敵キャラの状態を取得するためのメソッド
+    public State GetState()
+    {
+        return stateNo;
     }
 
     #region オノマトペ情報
