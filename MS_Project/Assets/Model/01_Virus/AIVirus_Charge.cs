a108fd1545aeda4c5b4afab6379e7c84febc3555
ohs10373 <114590340+ohs10373@users.noreply.github.com>
2024-12-03 19:18:26 +0900

    プレイヤーSE&敵ステータス変更&ウイルス&騎士AI更新
    敵は倒しやすくなり
    ウイルス、騎士は歩かないバグを直した

--- MS_Project/Assets/Model/01_Virus/AIVirus_Charge.cs
+++ MS_Project/Assets/Model/01_Virus/AIVirus_Charge.cs
@@ -7,12 +7,69 @@ public class AIVirus_Charge : EnemyAction
     [SerializeField, Header("突進スピード")]
     public float chargeSpeed = 60.0f;
 
+    private Vector3 direction;
+
     // 前にツッコむ
     private void SlimeCharge()
     {
         float chargeForce = enemy.RigidBody.mass * chargeSpeed;
         enemy.RigidBody.AddForce(enemy.transform.forward * chargeForce, ForceMode.Impulse);
     }
+
+    public void WalkInit()
+    {
+        enemy.Anim.Play("Walk");
+    }
+
+    public void WalkTick()
+    {
+        //ダメージチェック
+        if (stateHandler.CheckHit()) return;
+
+        enemy.Move();
+
+        // enemyの現在の角度を基にした前方向
+        Vector3 forwardDirection = enemy.transform.forward;
+        forwardDirection.y = 0f;// 地面に沿った移動
+
+        //適度に距離を置く
+        if (distanceToPlayer >= enemy.EnemyStatus.StatusData.attackDistance)
+        {
+            // 追跡
+            enemy.OnMovementInput?.Invoke(forwardDirection.normalized);
+        }
+        else if (distanceToPlayer < enemy.EnemyStatus.StatusData.attackDistance * 0.5f)
+        {
+            // 後ろ
+            enemy.OnMovementInput?.Invoke(-forwardDirection.normalized / 2.0f);
+        }
+
+        //ちょっとずつ見る
+        direction = player.position - enemy.transform.position;
+
+        Quaternion targetRotation = Quaternion.LookRotation(direction.normalized);
+        targetRotation.x = 0f;
+        targetRotation.z = 0f;
+
+        AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+
+        enemy.transform.rotation = Quaternion.Slerp(
+        enemy.transform.rotation,
+        targetRotation,
+        0.1f // 補間率（1.0fで即時、0.0fで変化なし）
+    );
+
+        //攻撃へ遷移
+        if (distanceToPlayer <= enemyStatus.StatusData.attackDistance && enemy.AllowAttack)
+        {
+            //クールダウン
+            enemy.StartAttackCoroutine();
+
+            stateHandler.TransitionState(ObjectStateType.Attack);
+            return;
+        }
+    }
+
 
     #region オノマトペ情報
     private void VirusWalkData()
