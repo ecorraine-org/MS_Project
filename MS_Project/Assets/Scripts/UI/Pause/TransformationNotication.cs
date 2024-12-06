ea7027c47cff30764cb99aac4b7aa4f533834d8f
KawazoeYusuk <githubyou.hub@gmail.com>
2024-12-06 19:48:47 +0900

    Pause画面をつくりました

---
+++ MS_Project/Assets/Scripts/UI/Pause/TransformationNotication.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformationNotication : MonoBehaviour
+{
+    [SerializeField,Header("通知BOX")]
+    GameObject transformationnotication;
+
+    float cunt;
+
+    //フレームを60に固定
+    private void Start()
+    {
+        //Application.targetFrameRate = 60;
+    }
+    private void Update()
+    {
+        //if (Input.GetKeyDown(KeyCode.Return))
+        //{
+            cunt += Time.deltaTime;
+
+            //ゲームオブジェクト非表示→表示
+            //transformationnotication.SetActive(true);
+
+            Debug.Log(cunt);
+
+            //4秒後に消す処理
+            if (cunt >= 4)
+            {
+                //ゲームオブジェクト表示→非表示
+                transformationnotication.SetActive(false);
+            }
+        //}
+    }
+}
