b1e825a63189fa2a8c1c6c22bc3d0d2e3e473d8d
zxc148995 <zxc148995@163.com>
2024-12-07 02:05:49 +0900

    Update: 剣スキルのエフェクト

--- MS_Project/Assets/Scripts/Character/Player/Bullet/WindBlade.cs
+++ MS_Project/Assets/Scripts/Character/Player/Bullet/WindBlade.cs
@@ -4,6 +4,32 @@ using UnityEngine;
 
 public class WindBlade : SpriteBullet
 {
+    ParticleManager particleManager;
 
-  
+    [SerializeField, Header("エフェクトスケール")]
+    float windBladeScale =0.5f;
+
+    public override void Init(bool _isFlipX)
+    {
+        base.Init(_isFlipX);
+        
+        spriteRenderer.flipX = !_isFlipX;
+        //親オブジェクトの向きによる反転処理
+        Vector3 currentEulerAngles = transform.rotation.eulerAngles;
+        if (_isFlipX)
+        {
+            transform.rotation = Quaternion.Euler(currentEulerAngles.x, -currentEulerAngles.y, currentEulerAngles.z);
+        }
+
+        float randomZRotation = Random.Range(-70f, 70f);
+        //ランダム回転
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, randomZRotation);
+
+
+        //パーティクル処理
+        particleManager = GetComponentInChildren<ParticleManager>();
+        particleManager.ChangeScale(windBladeScale);
+}
+
+
 }
