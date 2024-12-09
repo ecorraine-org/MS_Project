caa887ea1d383f556cbef30b9522fc6e894d9e5d
893aiueo <aiueoon700@gmail.com>
2024-12-09 19:55:33 +0900

    Fix LookAtOffset
    *LookAtの場所を修正

--- MS_Project/Assets/Scripts/Camera/CameraInputHandler.cs
+++ MS_Project/Assets/Scripts/Camera/CameraInputHandler.cs
@@ -8,7 +8,10 @@ public class CameraInputHandler : MonoBehaviour
     [SerializeField] private CameraController _cameraController;
     private void Update()
     {
-        //Lボタンを押すとロックオンカメラに切り替える
-
+        //Pボタンを押すとホワイトアウトエフェクトが発動する
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            //_cameraController.ExecuteEffect("WhiteoutEffect");
+        }
     }
 }
