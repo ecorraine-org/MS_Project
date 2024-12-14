530d4d6aee5a3e44b0ffb962c1b3913f86aa50aa
ohs10368 <ohs10368@osaka.hal.ac.jp>
2024-12-15 04:24:10 +0900

    プレイヤー死亡時にGameOverに遷移

---
+++ MS_Project/Assets/Scripts/GameOver/GameOver.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    public Image fadePanel;             // フェード用のUIパネル（Image）
+    public float fadeDuration = 1.0f;   // フェードの完了にかかる時間
+    public Image titlePanel;
+    public Image continuePanel;
+
+    bool Title;
+    bool Continue;
+
+    [SerializeField] string sceneToLoad; // 切り替えるシーン名を指定
+
+    void Start()
+    {
+        fadePanel.enabled = false;       // フェードパネルを無効化
+        fadePanel.color = new Color(fadePanel.color.r, fadePanel.color.g, fadePanel.color.b, 0.0f); // 初期状態では透明
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Continue = false;
+            Title = true;
+        }
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            Title = false;
+            Continue = true;
+        }
+
+        if(Title)
+        {
+            titlePanel.color = new Color(255, 0, 0, 255);
+        }
+        if(Continue)
+        {
+            continuePanel.color = new Color(255, 0, 0, 255);
+        }
+
+        if ((!Title))
+        {
+            titlePanel.color = new Color(255, 255, 255, 255);
+        }
+        if(!Continue)
+        {
+            continuePanel.color = new Color(255, 255, 255, 255);
+        }
+
+        if(Title && Input.GetKeyDown(KeyCode.Return))
+        {
+            StartCoroutine(FadeOutAndLoadScene());
+        }
+    }
+
+    public IEnumerator FadeOutAndLoadScene()
+    {
+        fadePanel.enabled = true;   // フェードパネルを有効化s
+
+        float elapsedTime = 0.0f;                        // 経過時間を初期化
+        Color startColor = fadePanel.color;              // フェードパネルの開始色を取得
+        Color endColor = new Color(startColor.r, startColor.g, startColor.b, 1.0f); // フェードパネルの最終色を設定
+
+        // フェードアウトアニメーションを実行
+        while (elapsedTime < fadeDuration)
+        {
+            elapsedTime += Time.deltaTime;                        // 経過時間を増やす
+            float t = Mathf.Clamp01(elapsedTime / fadeDuration);  // フェードの進行度を計算
+            fadePanel.color = Color.Lerp(startColor, endColor, t); // パネルの色を変更してフェードアウト
+            yield return null;                                     // 1フレーム待機
+        }
+
+        fadePanel.color = endColor;                                // フェードが完了したら最終色に設定
+        SceneManager.LoadScene(sceneToLoad);                    // シーンをロードしてメニューシーンに遷移
+    }
+}
